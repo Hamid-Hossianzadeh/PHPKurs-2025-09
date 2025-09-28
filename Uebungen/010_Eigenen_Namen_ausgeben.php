7eeaa6c2e8adbfe8e568b4b77a5e2a38aea7b20e
Hamid Hossianzadeh <hamid.hossianzadeh@gmail.com>
2025-09-28 22:14:13 +0200

    php

--- Uebungen/010_Eigenen_Namen_ausgeben.php
+++ Uebungen/010_Eigenen_Namen_ausgeben.php
@@ -1,16 +1,10 @@
 <?php
+
 /*
-* Eigenen Namen ausgeben
-*
-* Erstelle je eine Variable für Vornamen,
-* Nachnamen und Wohnort und gib dann folgendes aus:
-* X Y wohnt in Z.
-*/
+ * Eigenen Namen ausgeben
+ *
+ * Erstelle je eine Variable für Vornamen,
+ * Nachnamen und Wohnort und gib dann folgendes aus:
+ * X Y wohnt in Z.
+ */
 
-// Variablen erstellen
-$vorname = "Hamid";
-$nachname = "Hossianzadeh";
-$wohnort = "Heilbronn";
-
-// Ausgabe
-echo $vorname . " " . $nachname . " wohnt in " . $wohnort . ".";
--- Uebungen/030_wuerfeln.php
+++ Uebungen/030_wuerfeln.php
@@ -1,25 +1,14 @@
 <?php
+
 /*
-* Würfeln
-*
-* Würfel eine Zahl (von 1 bis 6) und gib aus,
-* was gewürfelt wurde:
-* Super, eine 6!
-* Immerhin noch eine 5!
-* Das reicht nicht! (bei 1-4)
-*
-* Hilfsmittel: rand(), Verzweigung
-*/
+ * Würfeln
+ *
+ * Würfel eine Zahl (von 1 bis 6) und gib aus,
+ * was gewürfelt wurde:
+ * Super, eine 6!
+ * Immerhin noch eine 5!
+ * Das reicht nicht! (bei 1-4)
+ *
+ * Hilfsmittel: rand(), Verzweigung
+ */
 
-
-// Würfeln einer Zahl zwischen 1 und 6
-$zahl = rand(1, 6);
-
-// Ausgabe abhängig vom Ergebnis
-if ($zahl == 6) {
-    echo "Super, eine 6!";
-} elseif ($zahl == 5) {
-    echo "Immerhin noch eine 5!";
-} else {
-    echo "Das reicht nicht! (bei $zahl)";
-}
